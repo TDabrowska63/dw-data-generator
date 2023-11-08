78aa852c4ee277ec358f5343f9c665f23ad50887
Mikołaj Bisewski <s188594@o365.student.pg.edu.pl>
2023-11-08 20:13:10 +0100

    wrote create zgloszenia.py

---
+++ generate_csv/create_zgloszenia.py
@@ -0,0 +1,8 @@
+from generate_csv.zgloszenia import Zgloszenia
+
+def generate_zgloszenia(num_of_records:int):
+    with open('./create_csv/ports.csv', 'w' , encoding="utf-8") as file:
+        file.write("Id_pracownika;Data_wpisu;Godzina_wpisu;Imie;Nazwisko;Nr_telefonu;Id_Zgloszenia;Zglaszany_numer_rejestracyjny;Powod;Potwierdzone\n")
+        for i in range(num_of_records):
+            zgloszenie = Zgloszenia(i)
+            file.write(str(zgloszenie))
