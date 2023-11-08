21c455f754e463a395ea0f488a95b86333bb7d5a
Tysia <tyska.dabrowska@gmail.com>
2023-11-08 21:29:15 +0100

    tools file

--- generator.py
+++ generator.py
@@ -1,11 +1,14 @@
 from tables.samochody import Samochod
 from tables.uzytkownicy import Uzytkownik
+from tables.wypozyczenia import Wypozyczenie
 
 
 class Generator:
     def __init__(self):
         samochod = Samochod()
         uzytkownik = Uzytkownik()
+        wypozyczenie = Wypozyczenie(1, 300)
         print(samochod)
         print(uzytkownik)
+        print(wypozyczenie)
 
---
+++ tools.py
@@ -0,0 +1,9 @@
+from random import randrange
+from datetime import datetime, timedelta
+
+
+def random_date(start, end):
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
