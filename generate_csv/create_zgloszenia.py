6746c337bb443478d75674459bcd5bd1bfebd4bb
Mikołaj Bisewski <s188594@o365.student.pg.edu.pl>
2023-11-08 20:19:49 +0100

    add zgloszenia.csv and change directory on create zgloszenia

--- generate_csv/create_zgloszenia.py
+++ generate_csv/create_zgloszenia.py
@@ -1,7 +1,7 @@
 from generate_csv.zgloszenia import Zgloszenia
 
 def generate_zgloszenia(num_of_records:int):
-    with open('./create_csv/ports.csv', 'w' , encoding="utf-8") as file:
+    with open('./generate_csv/zgloszenia.csv', 'w' , encoding="utf-8") as file:
         file.write("Id_pracownika;Data_wpisu;Godzina_wpisu;Imie;Nazwisko;Nr_telefonu;Id_Zgloszenia;Zglaszany_numer_rejestracyjny;Powod;Potwierdzone\n")
         for i in range(num_of_records):
             zgloszenie = Zgloszenia(i)
