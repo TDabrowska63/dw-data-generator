44500766d1d3b0783d2bb6d0a551d4382bf5b715
Tysia <tyska.dabrowska@gmail.com>
2023-11-09 00:00:59 +0100

    creating license plate generator in tools

--- tables/samochody.py
+++ tables/samochody.py
@@ -1,22 +1,14 @@
 import random
+from tools import create_license_plate_num
+
 
 class Samochod:
 
     def __init__(self):
-        self.nr_rejestracyjny = "G"
+        self.nr_rejestracyjny = create_license_plate_num()
         self.marka =  random.choice(["Reno", "Dacia", "Tesla", "Audi"])
         self.typ = random.choice(["Osobowy", "Dostawczy"])
-        self.create_license_plate_num()
 
-    def create_license_plate_num(self):
-        option = random.choice(["2_digit_region", "3_digit_region"])
-        if option == "2_digit_region":
-            self.nr_rejestracyjny += random.choice('DAS')
-        elif option == "3_digit_region":
-            self.nr_rejestracyjny += random.choice(["SP", "BY", "CH", "CZ", "DA", "KA", "KS", "KW", "LE", "MB", "ND"])
-        self.nr_rejestracyjny += " "
-        for _ in range(5):
-            self.nr_rejestracyjny += random.choice('ACEFGHJKLMNPRSTUWXYZ0123456789')
 
     def __str__(self):
         return f"{str(self.nr_rejestracyjny)}|" \
--- tools.py
+++ tools.py
@@ -1,9 +1,21 @@
 from random import randrange
 from datetime import datetime, timedelta
-
+import random
 
 def random_date(start, end):
     delta = end - start
     int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
     random_second = randrange(int_delta)
     return start + timedelta(seconds=random_second)
+
+def create_license_plate_num():
+    nr_rejestracyjny = "G"
+    option = random.choice(["2_digit_region", "3_digit_region"])
+    if option == "2_digit_region":
+        nr_rejestracyjny += random.choice('DAS')
+    elif option == "3_digit_region":
+        nr_rejestracyjny += random.choice(["SP", "BY", "CH", "CZ", "DA", "KA", "KS", "KW", "LE", "MB", "ND"])
+    nr_rejestracyjny += " "
+    for _ in range(5):
+        nr_rejestracyjny += random.choice('ACEFGHJKLMNPRSTUWXYZ0123456789')
+    return nr_rejestracyjny
