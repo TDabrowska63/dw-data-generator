34f3a785451c9e03629169c3448b2778448b9599
Tysia <tyska.dabrowska@gmail.com>
2023-11-09 10:35:43 +0100

    create_city tool

--- tables/uzytkownicy.py
+++ tables/uzytkownicy.py
@@ -1,16 +1,15 @@
 import names
 import random
 from geonamescache import GeonamesCache
+from tools import create_city
 
 class Uzytkownik:
     def __init__(self):
         self.nr_prawa_jazdy = ""
         self.imie = names.get_first_name()
         self.nazwisko = names.get_last_name()
-        self.miasto_zamieszkania = ""
+        self.miasto_zamieszkania = create_city().get('name')
         self.create_driving_license_num()
-        self.create_city()
-        self.miasto_zamieszkania = self.create_city()
 
 
     def create_driving_license_num(self):
@@ -19,19 +18,6 @@ class Uzytkownik:
                 self.nr_prawa_jazdy += "/"
             else:
                 self.nr_prawa_jazdy += str(random.randint(0,9))
-
-    def create_city(self):
-        gc = GeonamesCache()
-        cities = gc.get_cities()
-        pomorskie_cities = []
-        for city in cities.values():
-            if city.get('countrycode') == 'PL' and city.get('admin1code') == '82':
-                pomorskie_cities.append(city)
-        if pomorskie_cities:
-            city = random.choice(pomorskie_cities)
-        else:
-            city = random.choice(list(cities.values()))
-        return city.get('name')
 
 
     def __str__(self):
--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 from random import randrange
 from datetime import datetime, timedelta
 import random
+from geonamescache import GeonamesCache
 
 def random_date(start, end):
     delta = end - start
@@ -19,3 +20,16 @@ def create_license_plate_num():
     for _ in range(5):
         nr_rejestracyjny += random.choice('ACEFGHJKLMNPRSTUWXYZ0123456789')
     return nr_rejestracyjny
+
+def create_city():
+    gc = GeonamesCache()
+    cities = gc.get_cities()
+    pomorskie_cities = []
+    for city in cities.values():
+        if city.get('countrycode') == 'PL' and city.get('admin1code') == '82':
+            pomorskie_cities.append(city)
+    if pomorskie_cities:
+        city = random.choice(pomorskie_cities)
+    else:
+        city = random.choice(list(cities.values()))
+    return city
